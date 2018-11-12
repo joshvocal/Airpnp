4839c585ff73ccc3d211afcd6839b9274afdcc75
Josh Vocal <joshvocal@gmail.com>
2018-11-12 08:14:10 -0800

    add

--- client/app/src/main/java/joshvocal/me/client/api/service/MarkerClient.java
+++ client/app/src/main/java/joshvocal/me/client/api/service/MarkerClient.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 
 public interface MarkerClient {
 
-    public static final String BASE_URL = "http://32ee96ad.ngrok.io/";
+    public static final String BASE_URL = "http://192.168.1.78:3000/";
 
     @GET("markers")
     Call<List<Marker>> getMarkers();
